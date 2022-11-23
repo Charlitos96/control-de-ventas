fa3a74803316c52745e01f7d5ab1f3b9af4066f4
Charlitos96 <carlos_conejo_21@hotmail.com>
2022-11-23 16:03:47 -0700

    Add loginView and home View

--- src/main/java/com/example/consultadeventas/LoginController.java
+++ src/main/java/com/example/consultadeventas/LoginController.java
@@ -1,13 +1,19 @@
 package com.example.consultadeventas;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class LoginController {
 
-    @GetMapping
-    public String Login(){
-        return "Hola Mundo";
+    @GetMapping("/login")
+    public String sayHello(@RequestParam(value = "user", defaultValue = "Usuario") String name) {
+        return String.format("Validando: %s, espere por favor...", name);
+    }
+
+    @GetMapping("/home")
+    public String goHome(){
+        return "home";
     }
 }
